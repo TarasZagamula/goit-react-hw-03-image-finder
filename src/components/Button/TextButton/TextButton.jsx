1e9dfee5dbf6a76e8dcc25cb6d94e0d32396e5c0
TarasZagamula <jogoogidi@gmail.com>
2022-10-21 01:37:58 +0300

    123

--- src/components/Button/TextButton/TextButton.jsx
+++ src/components/Button/TextButton/TextButton.jsx
@@ -1,6 +1,4 @@
 import { TextButtonStyled } from './TextButton.styled';
-import PropTypes from 'prop-types';
-import { Children } from 'react';
 
 export const TextButton = ({ onClick, children }) => {
   return (
